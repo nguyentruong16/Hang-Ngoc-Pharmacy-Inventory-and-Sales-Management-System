d09b428b7698e91a13bf9e7e13e374640b1a356c
nguyentruong16 <dangtruong1655@gmail.com>
2026-06-14 20:16:32 +0700

    branch list v1

--- src/main/java/com/example/project/repository/BranchRepository.java
+++ src/main/java/com/example/project/repository/BranchRepository.java
@@ -2,6 +2,12 @@ package com.example.project.repository;
 
 import com.example.project.entity.Branch;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BranchRepository extends JpaRepository<Branch, Integer> {
+
+    @Query("SELECT b FROM Branch b LEFT JOIN FETCH b.statusID ORDER BY b.id")
+    List<Branch> findAllWithStatus();
 }
