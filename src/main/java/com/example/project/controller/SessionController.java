0096ce726542a44f3ca2cc9cf36878b9fb6f681f
DO NGOC DUC <doduc23555@gmail.com>
2026-06-26 21:52:26 +0700

    redirect user when role changed

---
+++ src/main/java/com/example/project/controller/SessionController.java
@@ -0,0 +1,16 @@
+package com.example.project.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/session")
+public class SessionController {
+
+    @GetMapping("/check")
+    public ResponseEntity<Void> check() {
+        return ResponseEntity.ok().build();
+    }
+}
